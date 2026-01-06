39606f96bcd507771ef10ade6ab2b332432c3177
amaechina-ikechukwu <amaechinaikechukwu6@gmail.com>
2026-01-06 16:17:11 +0100

    fix: Corrected script path in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "knrog-server",
-      script: "server/src/index.ts",
+      script: "./server/src/index.ts",
       interpreter: "bun", // or "node"
       env: {
         NODE_ENV: "production",
